88e8ace0bc20758f6de6d2492fb8b454ee1bcc83
Owen <owen.staddon@icloud.com>
2025-06-09 20:19:08 +0100

    Cleans up code - for better readability

--- GuessTheNumberGameConsole/Program.cs
+++ GuessTheNumberGameConsole/Program.cs
@@ -1,53 +1,57 @@
-﻿Console.WriteLine("Guess the Numbers Game");
-Console.WriteLine("Are you ready to play? Press 'Y' for Yes or 'N' for No");
-
-bool startGame = false;
-
-while (true)
-{
-    var key = Console.ReadKey(true);
-    var keyValue = char.ToUpper(key.KeyChar);
-
-    if (keyValue == 'Y')
-    {
-        startGame = true;
-        break;
-    }
-    else if (keyValue == 'N')
-    {
-        startGame = false;
-        break;
-    }
-    else
-    {
-        Console.WriteLine("Invalid answer. Try again.");
-        Console.WriteLine("Are you ready to play? Press 'Y' for Yes or 'N' for No");
-    }
-}
+﻿bool startGame = DoYouWantToPlay();
 
 while (startGame)
 {
-    Random random = new Random();
-    var randomNumber = random.Next(1, 100);
-    int count = 0;
+    int randomNumber = GenerateRandomNumber();
+    int attempts = PlayRound(randomNumber);
 
+    Console.WriteLine($"You guessed it in {attempts} tries!");
+
+    startGame = AskToPlayAgain();
+}
+
+Console.WriteLine("Thanks for playing!");
+
+bool DoYouWantToPlay()
+{
+    Console.WriteLine("Are you ready to play? Press 'Y' for Yes or 'N' for No");
+    while (true)
+    {
+        var key = Console.ReadKey(true);
+        var keyValue = char.ToUpper(key.KeyChar);
+
+        if (keyValue == 'Y') return true;
+        if (keyValue == 'N') return false;
+
+        Console.WriteLine("Invalid answer. Try again.");
+    }
+}
+
+int GenerateRandomNumber()
+{
+    Random random = new Random();
+    return random.Next(1, 100);
+}
+
+int PlayRound(int targetNumber)
+{
     Console.WriteLine("I have selected a number between 1 and 99. Try to guess it!");
+    int count = 0;
 
     while (true)
     {
         Console.Write("Enter your guess: ");
         var input = Console.ReadLine();
 
-        if (int.TryParse(input, out var num))
+        if (int.TryParse(input, out var guess))
         {
             count++;
 
-            if (num == randomNumber)
+            if (guess == targetNumber)
             {
-                Console.WriteLine($"Congratulations! You guessed it in {count} tries.");
-                break;
+                return count;
             }
-            else if (num > randomNumber)
+            else if (guess > targetNumber)
             {
                 Console.WriteLine("Too high!");
             }
@@ -61,13 +65,20 @@ while (startGame)
             Console.WriteLine("Please enter a valid number.");
         }
     }
+}
 
+bool AskToPlayAgain()
+{
     Console.WriteLine("Play again? Press 'Y' for Yes or 'N' for No");
-    var playAgainKey = Console.ReadKey(true).KeyChar;
 
-    if (char.ToUpper(playAgainKey) != 'Y')
+    while (true)
     {
-        startGame = false;
-        Console.WriteLine("Thanks for playing!");
+        var key = Console.ReadKey(true);
+        var keyValue = char.ToUpper(key.KeyChar);
+
+        if (keyValue == 'Y') return true;
+        if (keyValue == 'N') return false;
+
+        Console.WriteLine("Invalid answer. Press 'Y' or 'N'.");
     }
 }
